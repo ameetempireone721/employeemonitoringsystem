69b751803ed15cef199535f9d9e7067477cdb95a
Marlou <eogs.marlou@gmail.com>
2025-01-12 21:45:35 +0800

    added new build

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -23,6 +23,7 @@ export const AuthProvider = ({ children }) => {
     const logout = () => {
         setUser(null);
         localStorage.removeItem('user');
+        localStorage.removeItem('token');
         navigate('/login'); // Redirect to login page
     };
 
